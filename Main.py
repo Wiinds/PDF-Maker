e7c6e32addbdf7def9909c38ab3948e0a74540ba
Jeremy Abraham <jeremyabraham17@gmail.com>
2023-11-12 00:46:58 -0500

    Initial Commit for Generation PDF Documents with python

---
+++ Main.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+from fpdf import FPDF
+
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
+
+df = pd.read_csv("topics.csv")
+
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L")
+    pdf.line(10, 21, 200, 21)
+   
+    #set the Footer
+    pdf.ln(260)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")  
+    pdf.footer(row["Topic"])
+    
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
+        #set the Footer
+        pdf.ln(275)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+        
+    
+pdf.output("output.pdf")
+
+
