055a5ba0ec78c0cb4e1eb744670a9847864ec781
Jeremy Abraham <jeremyabraham17@gmail.com>
2023-11-13 00:58:14 -0500

    2nd commt,

--- Main.py
+++ Main.py
@@ -15,11 +15,11 @@ for index, row in df.iterrows():
     pdf.line(10, 21, 200, 21)
    
     #set the Footer
-    pdf.ln(260)
+    pdf.ln(270)
     pdf.set_font(family="Times", style="I", size=8)
     pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=10, txt=row["Topic"], align="R")  
-    pdf.footer(row["Topic"])
+    #pdf.footer(row["Topic"])
     
     for i in range(row["Pages"] - 1):
         pdf.add_page()
